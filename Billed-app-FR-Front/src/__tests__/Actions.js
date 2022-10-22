87a18b10150810c49ca92c7b4e3e964389534057
JK <jk.floods@gmail.com>
2022-10-22 19:31:10 +0200

    Update unit test for Action
    HTML modified with proper code (id duplicate)

--- Billed-app-FR-Front/src/__tests__/Actions.js
+++ Billed-app-FR-Front/src/__tests__/Actions.js
@@ -10,17 +10,24 @@ import '@testing-library/jest-dom/extend-expect'
 describe('Given I am connected as an Employee', () => {
   describe('When I am on Bills page and there are bills', () => {
     test(('Then, it should render icon eye'), () => {
-      const html = Actions()
+      const id = '47qAXb6fIm2zOKkLzMro';
+      const html = Actions(id)
+
       document.body.innerHTML = html
-      expect(screen.getByTestId('icon-eye')).toBeTruthy()
+
+      expect(screen.getByTestId('icon-eye47qAXb6fIm2zOKkLzMro')).toBeTruthy()
     })
   })
+
   describe('When I am on Bills page and there are bills with url for file', () => {
     test(('Then, it should save given url in data-bill-url custom attribute'), () => {
+      const id = '47qAXb6fIm2zOKkLzMro';
       const url = '/fake_url'
-      const html = Actions(url)
+      const html = Actions(id, url)
+
       document.body.innerHTML = html
-      expect(screen.getByTestId('icon-eye')).toHaveAttribute('data-bill-url', url)
+
+      expect(screen.getByTestId('icon-eye47qAXb6fIm2zOKkLzMro')).toHaveAttribute('data-bill-url', url)
     })
   })
 })
